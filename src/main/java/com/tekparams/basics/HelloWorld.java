627e452773fc5e8227549ff2873084add96f2806
navee <ntt.journeytoexcel@gmail.com>
2022-10-11 00:38:57 +0530

    test

---
+++ src/main/java/com/tekparams/basics/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.tekparams.basics;
+
+public class HelloWorld {
+    public void printHellowWorld(){
+                System.out.println("Hello World!");
+    }
+//    public static void main(String[] args){
+//        System.out.println("Hello World!");
+//    }
+}
---
+++ src/main/java/com/tekparams/main/Main.java
@@ -0,0 +1,17 @@
+package com.tekparams.main;
+
+import com.tekparams.inheritance.Developer;
+
+public class Main {
+    public static void main(String[] args){
+        /*
+        HelloWorld helloWorld = new HelloWorld();
+        Address address = new Address();
+        helloWorld.printHellowWorld();
+        address.printAddress();
+         */
+        //ArithermeticOperations arithermeticOperations = new ArithermeticOperations();
+        Developer obj1 = new Developer();
+        obj1.printDevloper();
+    }
+}
